6c7518569c73e21f95f7167457e24b4fe1070b1b
Arzu Hussein <arzu.huseyn@gmail.com>
2020-07-24 18:35:26 +0400

    updated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
+// Helper Functions
+function makeDone(e) {
+
+    let localArr = [];
+    let storedObj = JSON.parse(localStorage.getItem('todos'));
+    const data = $(e).parent().find('span').text();
+
+    for (let task in storedObj['datas']) {
+        if (data !== storedObj['datas'][task]) {
+            localArr.push(storedObj['datas'][task]);
+        }
+    }
+    storedObj['datas'] = localArr;
+
+    localStorage.setItem('todos', JSON.stringify(storedObj));
+
+    $(e).parent().find('span').css('text-decoration', 'line-through');
+
+}
+
+// jQuery Functions
 $(document).ready(() => {
 
     const todoInput = $('#todoInput');
     const todoSubmit = $('#todoSubmit');
     const todoList = $('.todo-items ul');
-    let todoDone = $('#todoDone');
 
     let storedObj = localStorage.getItem('todos');
 
@@ -14,63 +34,29 @@ $(document).ready(() => {
             let todo = `
             <li>
             <span id="todo-item">${newObj['datas'][task]}</span>
-            <button onclick="makeDone()" id="todoDone" class="btn btn-info">DONE</button>
+            <button onclick="makeDone(this)" id="todoDone" class="btn btn-info">DONE</button>
             </li>`;
             $(todo).appendTo(todoList);
         }
     }
 
-
-
     todoSubmit.on('click', () => {
         let todoObj = {
             'datas': [],
         };
-
         if (storedObj) {
             const newObj = JSON.parse(storedObj);
             newObj['datas'].push(todoInput.val());
             localStorage.setItem('todos', JSON.stringify(newObj));
-
         } else {
             todoObj['datas'].push(todoInput.val());
             localStorage.setItem('todos', JSON.stringify(todoObj));
         }
-
         let todo = `<li>
         <span>${todoInput.val()}</span>
-        <button id="todoDone" class="btn btn-info">DONE</button>
+        <button id="todoDone" onclick="makeDone(this)" class="btn btn-info">DONE</button>
         </li>`;
         $(todo).appendTo(todoList);
-
-
+        todoInput.val("");
     });
-
-
-
 });
-
-function makeDone() {
-
-    let localArr = [];
-    let storedObj = JSON.parse(localStorage.getItem('todos'));
-    const data = $(event.currentTarget).parent().find('span').text();
-
-    for(let task in storedObj['datas']) {
-        if(data!==storedObj['datas'][task]) {
-            localArr.push(storedObj['datas'][task]);
-        }
-    }
-    storedObj['datas'] = localArr;
-
-    localStorage.setItem('todos', JSON.stringify(storedObj));
-
-    console.log(data);
-    const done = `<li>
-        <del>${data}</del>
-        <button disabled id="todoDone" class="btn btn-info">DONE</button>
-        </li>`;
-    
-    $(event.currentTarget).parent().replaceWith(done);
-
-}
